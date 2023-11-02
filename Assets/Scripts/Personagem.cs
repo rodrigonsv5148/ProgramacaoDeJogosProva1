1ed6be1635f70b33e79b0b148eec87237c0be528
rodrigonsv5148 <rodrigonsv5148@gmail.com>
2023-11-02 15:00:03 -0300

    animacao basica completa
    Video 1 da descrição do código está completamente implementado

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -3,30 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*Tutoriais usados: 
+Animacao:
+        https://www.youtube.com/watch?v=9jgcEWnbHZA
+*/
+
 public class Personagem : MonoBehaviour
 {
     // Variaveis relacionadas ao status do personagem
     public float vidaPersonagem = 100f;
     [SerializeField]private float velocidadePersonagem = 5f;
     [SerializeField]private float forcaPulo = 600f;
+
     // Circulo da base do pe do personagem, serve para identificar colisor com o chao.
     [SerializeField]private float tamanhoCirculoBase = 2.5f;
 
     public bool machinima = false; 
+    private Animator animator;
 
     private Vector2 Input = new Vector2();
     private float horizontalInput;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private bool permitirPulo = false;
 
     [SerializeField] private Transform basePersonagem;
     [SerializeField] private LayerMask chaoLayer;
+    
+    //A unity Recomenda usar um hash como "variavel do animator" p melhorar desempenho
+    private int correndoHash = Animator.StringToHash("Correndo");
+    private int saltandoHash = Animator.StringToHash("Saltando");
 
     //Método de quando se inicia o jogo
     private void Awake ()
     {
         // Pega o componente rigidbody
         rb = GetComponent<Rigidbody2D>();
+
+        // Controle das animacoes
+        animator = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -43,6 +60,18 @@ public class Personagem : MonoBehaviour
     void Update()
     {
        permitirPulo = Physics2D.OverlapCircle(basePersonagem.position, tamanhoCirculoBase, chaoLayer);
+
+       // Codigo que controla as animacoes
+       animator.SetBool(correndoHash, horizontalInput != 0);
+       animator.SetBool(saltandoHash, !permitirPulo);
+
+       if(horizontalInput > 0)
+       {
+        sr.flipX = false;
+       }else if (horizontalInput < 0)
+       {
+        sr.flipX = true;
+       }
     }
     
     void OnMove(InputValue value)
