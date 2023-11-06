02763af70e096d53db7e9a65018bb8b85b89bfe0
OniHueAlloz <marinhoj07@gmail.com>
2023-11-06 08:59:23 -0300

    fala funcionante

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -8,21 +8,19 @@ public class NPC : MonoBehaviour
 {
 
     public Text fala1; 
-    public Text fala2;
 
     // Start is called before the first frame update
     void Start()
     {
         fala1.gameObject.SetActive(false);
-        fala2.gameObject.SetActive(false);
-        fala1.text = "Perdoe-nos por nossa hostilidade extrangeiro, somos escravos de um tireno...";
-        fala2.text = "O quê? Você vai nos ajudar? Então pegue a relíquia adiante e boa sorte.";
+        fala1.text = "Perdoe-nos por nossa hostilidade extrangeiro, somos escravos de um tirano... O quê? Você vai nos ajudar? Então pegue a relíquia adiante e boa sorte!";
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -33,21 +31,12 @@ public class NPC : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            {
-                fala1.gameObject.SetActive(false);
-                fala2.gameObject.SetActive(true);
-            }
-    }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player")) 
         {
-            // Hide the text message
-            fala2.gameObject.SetActive(false);
+            fala1.gameObject.SetActive(false);
         }
     }
 }
