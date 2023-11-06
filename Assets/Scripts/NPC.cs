639f517c264c6654c4475c632fce14eaf5b2b8e7
OniHueAlloz <marinhoj07@gmail.com>
2023-11-06 08:29:01 -0300

    falas

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public Text fala1; 
+public Text fala2;
 
 public class NPC : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        fala1.GameObject.SetActive(false);
+        fala2.GameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            fala1.GameObject.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                fala1.GameObject.SetActive(false);
+                fala2.GameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            // Hide the text message
+            fala.GameObject.SetActive(false);
+        }
     }
 }
