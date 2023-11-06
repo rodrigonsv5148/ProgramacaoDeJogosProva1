baa173c37d759ce7f03e90ea195eec8692654602
OniHueAlloz <marinhoj07@gmail.com>
2023-11-05 23:06:48 -0300

    Inimigo trigger

--- Assets/Scripts/MoveInimigo.cs
+++ Assets/Scripts/MoveInimigo.cs
@@ -7,6 +7,8 @@ public class MoveInimigo : MonoBehaviour
 
     public Transform player;
     public float velInimigo = 5.0f;
+    public float vidaInimigo = 3f;
+    public bool visao = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,7 @@ public class MoveInimigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player != null) 
+        if (player != null && visao) 
         {
             
             Vector3 direcaoDoJogador = player.position - transform.position;
@@ -29,6 +31,36 @@ public class MoveInimigo : MonoBehaviour
             Vector3 novaPosicao = transform.position + normalDirecao * velInimigo * Time.deltaTime;
 
             transform.position = novaPosicao;
+        }
+
+        if (vidaInimigo <= 0)
+        {
+            gameObject.SetActive(false); 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+             visao = true;
+        }
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+             visao = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+             visao = false;
         }
     }
 }
